fe0749bd972eecf99820778800683baab990349d
sargamsubedi <saragamsubedi998@gmail.com>
2026-08-13 23:18:55 +0545

    :initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-950">
+      <h1 className="text-5xl font-bold text-white">
+        learning tailwind css
+      </h1>
+    </div>
+  )
+}
+
+export default App
