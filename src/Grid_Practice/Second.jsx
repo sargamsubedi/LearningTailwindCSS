cf66744b0fbf2e4280c4aec53630139ff619ce2c
sargamsubedi <saragamsubedi998@gmail.com>
2026-08-17 23:47:25 +0545

    add second grid practice file

--- src/Grid_Practice/Second.jsx
+++ src/Grid_Practice/Second.jsx
@@ -5,13 +5,13 @@
 function Second()
 {
     return(
-        <div className="border w-1/2 grid grid-cols-3 gap-2 p-2">
-           <div className="border col-span-2">1</div>
-           <div className="border col-start-2 row-start-1 col-span-2">2</div>
-           <div className="border">3</div>
-           <div className="border">4</div>
-           <div className="border">5</div>
-           <div className="border">6</div>
+        <div className="border w-1/2 grid grid-cols-3 grid-rows-4 gap-2 p-2">
+           <div className="border p-10 bg-yellow-200 col-span-2 row-start-1 col-start-1">1</div>
+           <div className="border p-10 bg-yellow-200/50 col-start-2 row-start-1 col-span-2 row-span-2">2</div>
+           <div className="border p-10 bg-yellow-200 row-span-3">3</div>
+           <div className="border p-10 bg-yellow-200 -col-end-1">4</div>
+           <div className="border p-10 bg-yellow-200">5</div>
+           <div className="border p-10 bg-yellow-200">6</div>
 
         </div>
     )
